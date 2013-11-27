39fe8c0097267b92e9ac89179dab4d447158c3b6
killera <zhuanqingshan@163.com>
2013-11-27 23:31:09 +0800

    killera | Add pic

--- scientific_computation_and_visualization/section3-working-with-text/lesson2-textalignment.py
+++ scientific_computation_and_visualization/section3-working-with-text/lesson2-textalignment.py
@@ -99,5 +99,9 @@ t13 = ax.text(left, top, 'no rotation\n with newlines', horizontalalignment='cen
 
 objs = (ax, p, line, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13)
 print [each.get_zorder() for each in objs]
+
 ax.add_patch(p)
+
+plt.savefig('text alignment.png')
 plt.show()
+
