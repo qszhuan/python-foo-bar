41fbb57ef53b538fc593a0060d356fffc4e80561
killera <zhuanqingshan@163.com>
2013-11-30 01:11:03 +0800

    killera | generate overall and seperate graph for requests

--- scientific_computation_and_visualization/exercises/locust_trend.py
+++ scientific_computation_and_visualization/exercises/locust_trend.py
@@ -1,39 +1,53 @@
 # -*- coding:utf-8 -*-
+from datetime import datetime
 from itertools import groupby
 from optparse import OptionParser
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+def time_convert_func(str):
+    return datetime.strptime(str, '%Y-%m-%d %H:%M:%S,%f')
+
+
 def generate(file_name, img_file_name):
-    file_name = r'C:\perf-test-results\11-28-17.20\LOCUST02.2013-11-28.08.14.51.success.log.txt'
+    #file_name = r'C:\perf-test-results\11-28-17.20\LOCUST02.2013-11-28.08.14.51.success.log.txt'
+    file_name = r'/Users/zhuanqingshan/Dropbox/doc/graph/LOCUST02.2013-11-28.08.14.52.success.log.txt'
     img_file_name = file_name + '.png'
 
-    data = np.genfromtxt(fname=file_name, dtype=None, delimiter='|', names=('time', '', '', '', 'name', 'response_time', ''),
-                         comments=False, autostrip=True)
+    time_convert_func = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S,%f')
+
+    headers = ['time', 'label', 'loglevel', 'method', 'name', 'response_time', 'size']
+    dtype = [(headers[0], 'object')] + [(col, '|S32') for col in headers[1:-2]] + [(col, 'i') for col in headers[-2:]]
+    data = np.genfromtxt(fname=file_name,
+                         delimiter='|',
+                         autostrip=True,
+                         dtype=dtype,
+                         names=headers,
+                         converters={'time': time_convert_func})
 
     sorted_data = np.sort(data, order=['name', 'time'])
 
     grouped_name = groupby(sorted_data['name'])
 
-    requests_counts = [[key, len(list(group))] for key, group in grouped_name]
+    requests_counts = np.array([[key, len(list(group))] for key, group in grouped_name])
 
     lens = len(requests_counts)
     start_index = 0
+    plt.grid(True)
     for index, request in enumerate(requests_counts):
-        if index>2:
-            break
         name, count = request
+        count = int(count)
         #plt.subplot(1, 1, 11)
         #plt.title(request[0])
         yaxis = sorted_data['response_time'][start_index:start_index + count]
-        plt.plot(range(len(yaxis)), yaxis)
+        xaxis = sorted_data['time'][start_index:start_index + count]
+        plt.plot(xaxis, yaxis, 'o')
         start_index += count
 
-    xtime = len(set(data['time']))
-    plt.xlabel([0, xtime, 0, 50000])
-
-
+    plt.legend(requests_counts[:,0])
+    plt.savefig(file_name + '.'+name + '.png', dpi=1024)
+    #plt.clf()
 
     print data['time'], data['name'], data['response_time']
 
