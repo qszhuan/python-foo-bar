9747beb408387b4a1e06b818ecbb75369efb9286
tiger-dev-107 <TW-DIS-008@gmail.com>
2013-11-29 16:19:08 +0800

    update

--- scientific_computation_and_visualization/exercises/locust_trend.py
+++ scientific_computation_and_visualization/exercises/locust_trend.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def generate(file_name, img_file_name):
-    file_name = '/Users/zhuanqingshan/Dropbox/doc/graph/LOCUST02.2013-11-28.08.14.51.success.log.txt'
+    file_name = r'C:\perf-test-results\11-28-17.20\LOCUST02.2013-11-28.08.14.51.success.log.txt'
     img_file_name = file_name + '.png'
 
     data = np.genfromtxt(fname=file_name, dtype=None, delimiter='|', names=('time', '', '', '', 'name', 'response_time', ''),
@@ -14,9 +14,27 @@ def generate(file_name, img_file_name):
 
     sorted_data = np.sort(data, order=['name', 'time'])
 
-    name_group = groupby(sorted_data['name'])
-    name_list = list(name_group)
-    
+    grouped_name = groupby(sorted_data['name'])
+
+    requests_counts = [[key, len(list(group))] for key, group in grouped_name]
+
+    lens = len(requests_counts)
+    start_index = 0
+    for index, request in enumerate(requests_counts):
+        if index>2:
+            break
+        name, count = request
+        #plt.subplot(1, 1, 11)
+        #plt.title(request[0])
+        yaxis = sorted_data['response_time'][start_index:start_index + count]
+        plt.plot(range(len(yaxis)), yaxis)
+        start_index += count
+
+    xtime = len(set(data['time']))
+    plt.xlabel([0, xtime, 0, 50000])
+
+
+
     print data['time'], data['name'], data['response_time']
 
 
@@ -28,4 +46,3 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
     img_file_name = '.'.join([options.csv_file_name, 'png'])
     generate(options.csv_file_name, img_file_name)
-
